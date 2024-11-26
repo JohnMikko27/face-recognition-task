d5471f68fba7211fb7d83277629857a1a7613081
Mikko <realmiks27@gmail.com>
2024-11-25 17:28:41 -0800

    add ability to capture webcam image

--- src/components/customwebcam.tsx
+++ src/components/customwebcam.tsx
@@ -1,8 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState, useCallback } from "react";
 import Webcam from "react-webcam";
 
-export default function CustomWebcam() {
+export default function CustomWebcam({ webcamRef }: { webcamRef: any}) {
+  
+  const [imgSrc, setImgSrc] = useState<any[]>([]);
+
+  const capture = useCallback(() => {
+    const newImage = webcamRef.current.getScreenshot();
+    setImgSrc([...imgSrc, newImage]);
+
+  }, [webcamRef]);
 
   return (
-      <Webcam height={600} width={600} />
+    <div className="">
+      <Webcam height={600} width={600} ref={webcamRef} className="border-2 border-black border-solid"/>
+      <div className="btn-container">
+        <button onClick={capture}>Capture photo</button>
+      </div>
+      { imgSrc.length !== 0 && imgSrc.map((image) => {
+        return <img src={image}/>
+      })}
+    </div>
   );
 };
